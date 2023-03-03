357597b483430c91245fc1a2e9ceb1ddb5a16058
Tony Chow <tchow9@gmail.com>
2023-03-03 06:55:36 -0800

    Add edge case if shorten URL does not exist

--- express_server.js
+++ express_server.js
@@ -1,17 +1,16 @@
-const e = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
-  return Math.random().toString(36).slice(2, 8)
-}
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(2, 8);
+};
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -29,26 +28,36 @@ app.get('/urls', (req, res) => {
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
-
+/*
+Displays the long and short URL.
+*/
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { 
-    id: req.params.id, 
+  const templateVars = {
+    id: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
-  res.render('urls_show', templateVars)
+  res.render('urls_show', templateVars);
 });
-
+/*
+This post will generate a shortenURLKey and log it into the database. It will the redirect you to the URL
+*/
 app.post('/urls', (req, res) => {
   console.log(req.body);// Log the POST request body to the console
-  let shortenURLKey = generateRandomString()
-  urlDatabase[shortenURLKey] = req.body.longURL
-  res.redirect(`/urls/${shortenURLKey}`);// Respond with 'Ok' (we will replace this)
+  let shortenURLKey = generateRandomString();
+  urlDatabase[shortenURLKey] = req.body.longURL;
+  res.redirect(`/urls/${shortenURLKey}`);// Redirects you to the shortenURLKey
 });
-
+/*
+This will determine if the short url ID exist, if it doesnt, it will throw an error block to a new urls_error.ejs file i created specifcally made for errors.
+*/
 app.get('/u/:id', (req, res) => {
-  longURL = urlDatabase[req.params.id]
-  res.redirect(longURL)
-})
+  let longURL = urlDatabase[req.params.id];
+  if (longURL !== undefined) {
+    res.redirect(longURL);
+  } else {
+    res.render('urls_error', {Error: 'block'});
+  }
+});
 
 app.get('/hello', (req, res) => {
   res.send('<html><body> Hello <b>World</b></body></html>\n');
